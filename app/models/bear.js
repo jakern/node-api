ed6c0264b0eba770fdf6b016e7674fffc72ace8b
Jacob Kern <jakern@users.noreply.github.com>
2017-02-21 15:47:52 -0500

    Update bear.js
    move to bears to NPC

--- app/models/bear.js
+++ app/models/bear.js
@@ -1,8 +1,8 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var BearSchema   = new Schema({
+var NPCSchema   = new Schema({
 	name: String
 });
 
-module.exports = mongoose.model('Bear', BearSchema);
+module.exports = mongoose.model('NPC', NPCSchema);
